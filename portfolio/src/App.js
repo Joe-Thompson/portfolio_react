7d715549c74e391f76854eeb98feea5135c01d5f
Joe-Thompson <bmo3696@gmail.com>
2020-12-15 17:55:27 -0500

    contact form done

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import './css/index.css';
 import Landing from './components/Landing';
 import Nav from "./components/Nav";
+import ContactForm from "./components/Contact_Form";
 
 function App() {
   return (
     <div className="App">
         <Nav />
         <Landing />
+        <ContactForm />
     </div>
   );
 }
